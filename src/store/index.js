625f458a060f947a54d55449ba366f764113535c
Arizal Sykes <arizalinc@gmail.com>
2020-09-14 01:20:51 +0700

    ✨Added vuex

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Auth from './modules/auth'
+import Product from './modules/product'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    Auth,
+    Product
+  },
+  state: {
+    name: 'Arizal'
+  },
+  mutations: {},
+  actions: {},
+  getters: {}
+})
